b6d78e11a2208003215200f92c8305b9f8e8d876
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2020-03-07 20:38:14 +0200

    p4

---
+++ Protypes and Inheritance/lab-4.js
@@ -0,0 +1,86 @@
+class Figure{
+    units={
+        mm:10,
+        cm:1,
+        m:0.01
+    }
+
+    defaultUnit;
+
+    constructor(defaultUnit = 'cm'){
+        this.defaultUnit = defaultUnit;
+    }
+    get area(){}
+
+    changeUnits(newUnit){
+        this.defaultUnit = newUnit;
+    }
+
+    toString(){
+        return "Figures units:"
+    }
+}
+
+class Circle extends Figure{
+    radius = 0;
+
+    constructor(r, ...rest){
+        super(...rest);
+        this.radius = r;
+    }
+
+    get r(){
+        return this.radius * this.units[this.defaultUnit];
+    }
+
+    get area(){
+        return Math.PI * this.r * this.r;
+    }
+
+    toString(){
+        return super.toString() + `${this.defaultUnit} Area: ${this.area} - radius: ${this.r}`;
+    }
+}
+
+class Rectangle extends Figure{
+    width = 0;
+    height = 0;
+
+    constructor(w, h, ...rest){
+        super(...rest);
+        this.width = w,
+        this.height = h
+    }
+
+    get area(){
+        return this.w * this.h;
+    }
+
+    get w(){
+        return this.width * this.units[this.defaultUnit];
+    }
+
+    get h(){
+        return this.height * this.units[this.defaultUnit];
+    }    
+
+    toString(){
+        return super.toString() + `${this.defaultUnit} Area: ${this.area} - width: ${this.w}, height: ${this.h}`
+    }
+}
+
+let c = new Circle(5);
+console.log(c.area); // 78.53981633974483
+console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+
+let r = new Rectangle(3, 4, 'mm');
+console.log(r.area); // 1200 
+console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
+
+r.changeUnits('cm');
+console.log(r.area); // 12
+console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4
+
+c.changeUnits('mm');
+console.log(c.area); // 7853.981633974483
+console.log(c.toString()) // Figures units: mm Area: 7853.981633974483 - radius: 50
